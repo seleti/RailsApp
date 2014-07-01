d482ec0163fbe318ef008122e98c788218327dda
Siddartha Eleti <sid.eleti@me.com>
2014-07-01 12:20:26 -0400

    Initial Commit with SQLite

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsapp::Application.config.secret_key_base = '6ed34edf53efd66973c595a74189a99e069d8e20fc0ac4cfe50f0569948467016674c3a577a6a8f3574b6922d98e50570909fa9ffabf439503ceeb550f4d1741'
